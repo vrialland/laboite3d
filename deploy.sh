cee85205131cb20c9c57466f4cef1754ccae5502
Vincent Rialland <vincent.rialland@gmail.com>
2017-01-09 21:57:36 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-git checkout gh-pages
+git clone . out
 npm run-script build
+cp bundle.js out
+cd out
+git add .
 git rm -r src
-git add -f bundle.js
 git commit -am 'Deploy rev '$(git rev-parse HEAD)
-git push
-git reset --hard HEAD
-git co $branch
+git push origin master:gh-pages -f
+cd ..
+rm -rf out
